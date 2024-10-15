b07e6dd59b88551e3ef3908d31bfbd8b4783bf6e
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-15 19:12:14 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     ''' Coroutine that asynchronously yields
     random numbers between 0 and 10 '''
     for _ in range(10):
