68d0a63013ec75824394ca0cacb00ddff8f2cc36
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-15 20:11:33 +0000

    docstring

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,6 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Measure the total runtime"""
     start_time = time.time()
 
     tasks = [async_comprehension() for _ in range(4)]
