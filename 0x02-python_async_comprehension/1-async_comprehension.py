94c64b0c821a4d8df77768fd0aaf8517eb7cd319
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-15 19:25:59 +0000

    task 1: updated task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 ''' Async Comprehensions '''
 
-import asyncio
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     ''' Coroutine that asynchronously compress
     random numbers from async_generator '''
     return [val async for val in async_generator()]
